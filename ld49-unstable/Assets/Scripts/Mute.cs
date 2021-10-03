1e4be54d8febf9ef7a6985a00d606ee99f28669f
Alexandre Melotti <contato@lexmelotti.com>
2021-10-03 20:00:03 -0300

    Fixed mute button and added fullscreen to webplayer

--- ld49-unstable/Assets/Scripts/Mute.cs
+++ ld49-unstable/Assets/Scripts/Mute.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Mute : MonoBehaviour
 {
 	[SerializeField]
 	private AudioMixer _audioMixer;
+
+	private void Start()
+	{
+		var volume = 0f;
+		_audioMixer.GetFloat("musicVolume", out volume);
+
+		if (volume == -80)
+		{
+			GetComponent<Toggle>().isOn = true;
+		}
+	}
 
 	public void ToggleMute(bool value)
 	{
